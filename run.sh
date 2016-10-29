de6dc99e0de2341fa23fb674948aa0b330dfd702
Rafael Mohlin <raffe_mohlin@hotmail.com>
2016-10-29 13:54:45 +0200

    run.sh can use multiple Steam tokens

--- run.sh
+++ run.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-# Change to to the server account token
-# https://steamcommunity.com/dev/managegameservers
-token="..."
+if [ -z "$1" ]; then
+        echo "Usage: $0 <port>"
+        exit
+fi
+
+tokens=()
+while read line
+do
+        if [[ $line == \#* ]]; then
+                continue
+        fi
+        split=( $line )
+        echo ${split[0]}
+        echo ${split[1]}
+        tokens+=( [${split[0]}]=${split[1]} )
+done < tokens.txt
+token=${tokens[$1]}
+
+if [ -z "$token" ]; then
+        echo -n "WARNING! No Steam token set for port $1"
+        sleep 1
+        echo -n "."
+        sleep 1
+        echo -n "."
+        sleep 1
+        echo "."
+else
+        token="+sv_setsteamaccount $token"
+fi
 
 dir=$(dirname $0)
 cd "${dir}/csgo-ds"
@@ -10,8 +36,9 @@ export PATH=$PATH:/sbin
 exec ./srcds_run -game csgo \
     -console -usercon -autoupdate -tickrate 128 \
     -maxplayers 14 -maxplayers_override 14 \
-    +sv_setsteamaccount "${token}" \
+    $token \
     +ip 0.0.0.0 \
     +game_type "0" +game_mode "1" \
     +mapgroup mg_active \
     +map de_dust2
+
