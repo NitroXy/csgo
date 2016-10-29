e9a070e2b624a9405902bfe065be40da6e354e46
Rafael Mohlin <raffe_mohlin@hotmail.com>
2016-10-29 13:54:05 +0200

    Wrong folder

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 dir=$(dirname $0)
-exec "${dir}/steamcmd/steamcmd.sh" +login "anonymous" +force_install_dir "${dir}/csgo-ds" +app_update 740 validate +quit
+exec "${dir}/steamcmd/steamcmd.sh" +login "anonymous" +force_install_dir "${dir}/../csgo-ds" +app_update 740 validate +quit
